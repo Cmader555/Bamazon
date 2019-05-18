b9002ba5379f8c1d160f6d341771db572d55f46f
Christian Mader <Cmader555@users.noreply.github.com>
2019-05-17 22:46:48 -0700

    Set up new JS file for next task and connected db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,8 @@
 require("console.table")
 const inquirer = require("inquirer")
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
---
+++ bamazonManager.js
@@ -0,0 +1,17 @@
+require("console.table")
+const inquirer = require("inquirer")
+const mysql = require("mysql");
+
+let connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon_db"
+});
