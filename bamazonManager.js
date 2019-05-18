fbe35d9f682d2527f6923920f31cd9bd37ebd703
Christian Mader <Cmader555@users.noreply.github.com>
2019-05-17 23:12:42 -0700

    made prompt menu for manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -15,3 +15,29 @@ let connection = mysql.createConnection({
   password: "",
   database: "bamazon_db"
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    
+  });
+
+
+  function menu() {
+
+    inquirer.prompt({
+        name: "menu", 
+        type: "list", 
+        message: "What do you need to manage?", 
+        choices: [
+            "View Products For Sale", 
+            "View Low Inventory", 
+            "Add To Inventory", 
+            "Add New Product"
+
+        ]
+
+
+
+    }); 
+  }; 
